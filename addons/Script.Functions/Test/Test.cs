c45ff3b23e77b3701d2123d30182242e4b2600a4
Christopher <git@clatza.dev>
2021-02-05 08:28:43 +0100

    add empty module

---
+++ addons/Script.Functions/Test/Test.cs
@@ -0,0 +1,29 @@
+﻿using Chris.OS.Additions.Utils;
+using Mono.Addins;
+using OpenSim.Region.Framework.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chris.OS.Additions.Script.Functions.Test
+{
+    [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule", Id = "Test")]
+
+    class Test : EmptyModule
+    {
+        public override String Name
+        {
+            get
+            {
+                return "Test";
+            }
+        }
+
+        public override void RegionLoaded(Scene scene)
+        {
+            base.Logger.Info("========= TEST WORKED =========");
+        }
+    }
+}
---
+++ addons/Utils/EmptyModule.cs
@@ -0,0 +1,46 @@
+﻿using log4net;
+using Nini.Config;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scenes;
+using System;
+using System.Reflection;
+
+namespace Chris.OS.Additions.Utils
+{
+    class EmptyModule : INonSharedRegionModule
+    {
+        protected readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        protected Scene World = null;
+        protected Boolean Enabled = false;
+
+        public virtual string Name
+        {
+            get { return null; }
+        }
+
+        public virtual Type ReplaceableInterface
+        {
+            get { return null; }
+        }
+
+        public virtual void AddRegion(Scene scene)
+        {
+        }
+
+        public virtual void Close()
+        {
+        }
+
+        public virtual void Initialise(IConfigSource source)
+        {
+        }
+
+        public virtual void RegionLoaded(Scene scene)
+        {
+        }
+
+        public virtual void RemoveRegion(Scene scene)
+        {
+        }
+    }
+}
