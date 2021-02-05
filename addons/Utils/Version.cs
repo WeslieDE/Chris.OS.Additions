a65b9b195d3e061f79cad784904cec7068333de9
Chris <christopher@clatza.dev>
2021-02-05 04:26:03 +0100

    add utils

---
+++ addons/Utils/Version.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chris.OS.Additions.Utils
+{
+    class Version
+    {
+        public static int PLUGIN_VERSION = 0;
+    }
+}
