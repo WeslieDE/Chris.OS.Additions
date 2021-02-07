97120eb97e17da072cac81f0e6d848848ff2d575
Chris <christopher@clatza.dev>
2021-02-07 07:31:02 +0100

    cleanup

--- addons/Script.Functions/EasyDialog/EasyDialog.cs
+++ addons/Script.Functions/EasyDialog/EasyDialog.cs
@@ -9,13 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Timers;
-using LSL_Float = OpenSim.Region.ScriptEngine.Shared.LSL_Types.LSLFloat;
-using LSL_Integer = OpenSim.Region.ScriptEngine.Shared.LSL_Types.LSLInteger;
-using LSL_Key = OpenSim.Region.ScriptEngine.Shared.LSL_Types.LSLString;
-using LSL_List = OpenSim.Region.ScriptEngine.Shared.LSL_Types.list;
-using LSL_Rotation = OpenSim.Region.ScriptEngine.Shared.LSL_Types.Quaternion;
-using LSL_String = OpenSim.Region.ScriptEngine.Shared.LSL_Types.LSLString;
-using LSL_Vector = OpenSim.Region.ScriptEngine.Shared.LSL_Types.Vector3;
 
 namespace Chris.OS.Additions.Script.Functions.EasyDialog
 {
--- addons/Script.Functions/EasyDialog/EasyDialogEvents.cs
+++ addons/Script.Functions/EasyDialog/EasyDialogEvents.cs
@@ -1,9 +1,5 @@
 ﻿using OpenMetaverse;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Chris.OS.Additions.Script.Functions.EasyDialog
 {
