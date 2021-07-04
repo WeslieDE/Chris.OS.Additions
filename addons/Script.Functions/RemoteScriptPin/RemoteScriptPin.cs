3c468ede4beae37ef433767e0c9b377d331b61a9
Christopher <christopher@clatza.dev>
2021-07-04 18:18:59 +0200

    add RemoteScriptPin

---
+++ addons/Script.Functions/RemoteScriptPin/RemoteScriptPin.cs
@@ -0,0 +1,58 @@
+﻿using Chris.OS.Additions.Utils;
+using Mono.Addins;
+using OpenMetaverse;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chris.OS.Additions.Script.Functions.RemoteScriptPin
+{
+    [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule", Id = "OwnerSay")]
+
+    class RemoteScriptPin : EmptyModule
+    {
+        #region EmptyModule
+        public override string Name
+        {
+            get { return "RemoteScriptPin"; }
+        }
+
+        public override void RegionLoaded(Scene scene)
+        {
+            base.World = scene;
+
+            try
+            {
+                IScriptModuleComms m_scriptModule = base.World.RequestModuleInterface<IScriptModuleComms>();
+                m_scriptModule.RegisterScriptInvocation(this, "osRemoteScriptPin");
+            }
+            catch (Exception e)
+            {
+                base.Logger.WarnFormat("[" + Name + "]: script method registration failed; {0}", e.Message);
+            }
+        }
+        #endregion
+
+        #region Script functions
+        [ScriptInvocation]
+        public void osRemoteScriptPin(UUID hostID, UUID scriptID, UUID prim, int pin)
+        {
+            SceneObjectPart host = base.World.GetSceneObjectPart(hostID);
+            SceneObjectPart part = base.World.GetSceneObjectPart(prim);
+
+            if (part == null)
+                throw new Exception("Object not found!");
+
+            if(host.OwnerID != part.OwnerID)
+                throw new Exception("Owner dont match!");
+
+            part.ScriptAccessPin = pin;
+        }
+        #endregion
+
+    }
+}
