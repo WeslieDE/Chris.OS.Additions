08c012158aef184356af5f5213e050f1a23853e2
Christopher <git@clatza.dev>
2021-07-29 16:58:46 +0200

    start with pathfinder

---
+++ addons/Script.Functions/PathFinder/PathFinder.cs
@@ -0,0 +1,16 @@
+﻿using Chris.OS.Additions.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chris.OS.Additions.Script.Functions.PathFinder
+{
+    class PathFinder : EmptyModule
+    {
+        private const String NODE_TEXTURE = "921f6d16-90c8-4926-963b-4698ff107c29";
+
+
+    }
+}
