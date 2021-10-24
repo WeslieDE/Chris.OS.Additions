115e8b62b7a119bb8fcfe6c3958d37e81203e6a7
Christopher <christopher@clatza.dev>
2021-10-24 18:40:03 +0200

    use UUID for image id

--- addons/Script.Functions/LSLBitMap/LSLBitMap.cs
+++ addons/Script.Functions/LSLBitMap/LSLBitMap.cs
@@ -17,7 +17,7 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
 
     class LSLBitMap : EmptyModule
     {
-        private Dictionary<int, Bitmap> m_bitmaps = new Dictionary<int, Bitmap>();
+        private Dictionary<UUID, Bitmap> m_bitmaps = new Dictionary<UUID, Bitmap>();
 
         #region EmptyModule
         public override string Name
@@ -51,23 +51,17 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
 
         #region Script functions
         [ScriptInvocation]
-        public int osCreateBitmap(UUID hostID, UUID scriptID, int sizeX, int sizeY)
+        public UUID osCreateBitmap(UUID hostID, UUID scriptID, int sizeX, int sizeY)
         {
             lock(m_bitmaps)
             {
-                int newImageID = m_bitmaps.Count + 1;
+                UUID newImageID = UUID.Random();
 
-                if(sizeX == 0)
+                if (sizeX == 0)
                     sizeX = 1;
 
                 if(sizeY == 0)
                     sizeY = 1;
-
-                if (sizeX > 8192)
-                    sizeX = 8192;
-
-                if (sizeY > 8192)
-                    sizeY = 8192;
 
                 Bitmap bitmap = new Bitmap(sizeX, sizeY);
                 m_bitmaps.Add(newImageID, bitmap);
@@ -77,7 +71,7 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
         }
 
         [ScriptInvocation]
-        public int osLoadBitmap(UUID hostID, UUID scriptID, String inventoryName)
+        public UUID osLoadBitmap(UUID hostID, UUID scriptID, String inventoryName)
         {
             SceneObjectPart part = base.World.GetSceneObjectPart(hostID);
 
@@ -98,7 +92,7 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
                         {
                             lock (m_bitmaps)
                             {
-                                int newImageID = m_bitmaps.Count + 1;
+                                UUID newImageID = UUID.Random();
 
                                 Bitmap bitmap = jpegImageData.ExportBitmap();
                                 m_bitmaps.Add(newImageID, bitmap);
@@ -114,7 +108,7 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
         }
 
         [ScriptInvocation]
-        public int osLoadBitmapFromURL(UUID hostID, UUID scriptID, String url)
+        public UUID osLoadBitmapFromURL(UUID hostID, UUID scriptID, String url)
         {
             try
             {
@@ -125,7 +119,7 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
 
                     lock (m_bitmaps)
                     {
-                        int newImageID = m_bitmaps.Count + 1;
+                        UUID newImageID = UUID.Random();
 
                         Bitmap bitmap = new Bitmap(tempImage.Width, tempImage.Height);
                         m_bitmaps.Add(newImageID, bitmap);
@@ -136,69 +130,75 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
             }
             catch
             {
-                return 0;
+                return UUID.Zero;
             }
         }
 
         [ScriptInvocation]
-        public int osUnloadBitmap(UUID hostID, UUID scriptID, int bitmapID)
+        public int osUnloadBitmap(UUID hostID, UUID scriptID, String bitmapID)
         {
-            if (m_bitmaps.ContainsKey(bitmapID))
+            if(UUID.TryParse(bitmapID, out UUID imageID))
             {
-                m_bitmaps.Remove(bitmapID);
-                return 1;
+                if (m_bitmaps.ContainsKey(imageID))
+                {
+                    m_bitmaps.Remove(imageID);
+                    return 1;
+                }
             }
 
             return 0;
         }
 
         [ScriptInvocation]
-        public int osSaveBitmap(UUID hostID, UUID scriptID, int bitmapID, String name)
+        public int osSaveBitmap(UUID hostID, UUID scriptID, String bitmapID, String name)
         {
             SceneObjectPart part = base.World.GetSceneObjectPart(hostID);
 
-            if (m_bitmaps.TryGetValue(bitmapID, out Bitmap image))
+            if (UUID.TryParse(bitmapID, out UUID imageID))
             {
-                byte[] imageArray = OpenMetaverse.Imaging.OpenJPEG.EncodeFromImage(image, true);
+                if (m_bitmaps.TryGetValue(imageID, out Bitmap image))
+                {
+                    byte[] imageArray = OpenMetaverse.Imaging.OpenJPEG.EncodeFromImage(image, true);
 
-                AssetBase asset = new AssetBase();
-                asset.CreatorID = part.OwnerID.ToString();
-                asset.FullID = UUID.Random();
+                    AssetBase asset = new AssetBase();
+                    asset.CreatorID = part.OwnerID.ToString();
+                    asset.FullID = UUID.Random();
 
-                asset.Name = "Image from LSLBitmap Module.";
-                asset.Description = "https://github.com/Sahrea/Chris.OS.Additions";
-                asset.Temporary = false;
-                asset.Type = (int)AssetType.Texture;
-                asset.Data = imageArray;
-                String assetID = base.World.AssetService.Store(asset);
+                    asset.Name = "Image from LSLBitmap Module.";
+                    asset.Description = "https://github.com/Sahrea/Chris.OS.Additions";
+                    asset.Temporary = false;
+                    asset.Type = (int)AssetType.Texture;
+                    asset.Data = imageArray;
+                    String assetID = base.World.AssetService.Store(asset);
 
-                if (assetID == String.Empty)
-                    return 0;
+                    if (assetID == String.Empty)
+                        return 0;
 
-                TaskInventoryItem item = new TaskInventoryItem();
-                item.Name = name;
-                item.AssetID = UUID.Parse(assetID);
-                item.OwnerID = part.OwnerID;
-                item.CreatorID = part.OwnerID;
-                item.LastOwnerID = part.OwnerID;
+                    TaskInventoryItem item = new TaskInventoryItem();
+                    item.Name = name;
+                    item.AssetID = UUID.Parse(assetID);
+                    item.OwnerID = part.OwnerID;
+                    item.CreatorID = part.OwnerID;
+                    item.LastOwnerID = part.OwnerID;
 
-                item.Type = 0;
-                item.PermsMask = 581639;
-                item.NextPermissions = 581639;
-                item.CurrentPermissions = 581639;
-                item.EveryonePermissions = 581639;
+                    item.Type = 0;
+                    item.PermsMask = 581639;
+                    item.NextPermissions = 581639;
+                    item.CurrentPermissions = 581639;
+                    item.EveryonePermissions = 581639;
 
-                part.Inventory.AddInventoryItemExclusive(item, false);
-                part.SendFullUpdateToAllClients();
+                    part.Inventory.AddInventoryItemExclusive(item, false);
+                    part.SendFullUpdateToAllClients();
 
-                return 1;
+                    return 1;
+                }
             }
 
             return 0;
         }
 
         [ScriptInvocation]
-        public int osResizeBitmap(UUID hostID, UUID scriptID, int bitmapID, int sizeX, int sizeY)
+        public int osResizeBitmap(UUID hostID, UUID scriptID, String bitmapID, int sizeX, int sizeY)
         {
             if (sizeX > 8192)
                 sizeX = 8192;
@@ -208,11 +208,14 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
 
             lock(m_bitmaps)
             {
-                if (m_bitmaps.TryGetValue(bitmapID, out Bitmap bitmap))
+                if (UUID.TryParse(bitmapID, out UUID imageID))
                 {
-                    m_bitmaps.Remove(bitmapID);
-                    m_bitmaps.Add(bitmapID, new Bitmap(bitmap, sizeX, sizeY));
-                    return 1;
+                    if (m_bitmaps.TryGetValue(imageID, out Bitmap bitmap))
+                    {
+                        m_bitmaps.Remove(imageID);
+                        m_bitmaps.Add(imageID, new Bitmap(bitmap, sizeX, sizeY));
+                        return 1;
+                    }
                 }
             }
 
@@ -220,34 +223,40 @@ namespace Chris.OS.Additions.Script.Functions.LSLBitMap
         }
 
         [ScriptInvocation]
-        public Vector3 osGetBitmapPixel(UUID hostID, UUID scriptID, int bitmapID, int posX, int posY)
+        public Vector3 osGetBitmapPixel(UUID hostID, UUID scriptID, String bitmapID, int posX, int posY)
         {
-            if (m_bitmaps.TryGetValue(bitmapID, out Bitmap bitmap))
+            if (UUID.TryParse(bitmapID, out UUID imageID))
             {
-                Color pixelColor = bitmap.GetPixel(posX, posY);
+                if (m_bitmaps.TryGetValue(imageID, out Bitmap bitmap))
+                {
+                    Color pixelColor = bitmap.GetPixel(posX, posY);
 
-                return new Vector3(pixelColor.R, pixelColor.G, pixelColor.B);
+                    return new Vector3(pixelColor.R, pixelColor.G, pixelColor.B);
+                }
             }
 
             return new Vector3();
         }
 
         [ScriptInvocation]
-        public int osSetBitmapPixel(UUID hostID, UUID scriptID, int bitmapID, int posX, int posY, Vector3 color)
+        public int osSetBitmapPixel(UUID hostID, UUID scriptID, String bitmapID, int posX, int posY, Vector3 color)
         {
             lock(m_bitmaps)
             {
                 try
                 {
-                    if (m_bitmaps.TryGetValue(bitmapID, out Bitmap bitmap))
+                    if (UUID.TryParse(bitmapID, out UUID imageID))
                     {
-                        Color pixelColor = Color.FromArgb((int)color.X, (int)color.Y, (int)color.Z);
-                        bitmap.SetPixel(posX, posY, pixelColor);
+                        if (m_bitmaps.TryGetValue(imageID, out Bitmap bitmap))
+                        {
+                            Color pixelColor = Color.FromArgb((int)color.X, (int)color.Y, (int)color.Z);
+                            bitmap.SetPixel(posX, posY, pixelColor);
 
-                        m_bitmaps.Remove(bitmapID);
-                        m_bitmaps.Add(bitmapID, bitmap);
+                            m_bitmaps.Remove(imageID);
+                            m_bitmaps.Add(imageID, bitmap);
 
-                        return 1;
+                            return 1;
+                        }
                     }
                 }
                 catch
