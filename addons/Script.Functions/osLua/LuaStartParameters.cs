057186486a1c1a99d347c6a288fc364ec5208369
christopher <christopher@DESKTOP-EASV8FF>
2021-12-13 22:48:57 +0100

    add lua module

---
+++ addons/Script.Functions/osLua/LuaStartParameters.cs
@@ -0,0 +1,15 @@
+﻿using OpenSim.Region.Framework.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chris.OS.Additions.Script.Functions.osLua
+{
+    class LuaStartParameters
+    {
+        public SceneObjectPart Part = null;
+        public String LuaScript = null;
+    }
+}
