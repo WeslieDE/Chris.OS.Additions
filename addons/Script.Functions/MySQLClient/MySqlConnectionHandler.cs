d4f722eb606348081d18da8217416ba567233c04
Christopher <git@clatza.dev>
2022-06-16 13:29:13 +0200

    add lsl mysql client

---
+++ addons/Script.Functions/MySQLClient/MySqlConnectionHandler.cs
@@ -0,0 +1,148 @@
+﻿using Chris.OS.Additions.Utils;
+using MySql.Data.MySqlClient;
+using OpenMetaverse;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Chris.OS.Additions.Script.Functions.MySQLClient
+{
+    public class MySqlConnectionHandler
+    {
+        private MySqlCommand m_currentMySQLCommand = null;
+        private IDataReader m_currentDataReader = null;
+
+        public MySqlConnectionHandler(String connectionString, UUID handlerID)
+        {
+            m_connectionString = connectionString;
+            m_handlerID = handlerID;
+            m_lastUse = Tools.getUnixTime();
+
+            if (connectionString == String.Empty)
+                return;
+        }
+
+        #region GetSetHandler
+        private UUID m_handlerID = UUID.Zero;
+        public UUID HandlerID
+        {
+            get
+            {
+                return m_handlerID;
+            }
+        }
+
+        private String m_connectionString = null;
+        public String ConnectionString
+        {
+            get
+            {
+                return m_connectionString;
+            }
+        }
+
+        private MySqlConnection m_mySQLClient = null;
+        public MySqlConnection Conecction
+        {
+            get
+            {
+                return m_mySQLClient;
+            }
+        }
+
+        private int m_lastUse = 0;
+        public int LastUse
+        {
+            get
+            {
+                return m_lastUse;
+            }
+        }
+        #endregion
+
+        #region MySQLCommands
+        public void OpenConecction()
+        {
+            m_lastUse = Tools.getUnixTime();
+
+            m_mySQLClient = new MySqlConnection(m_connectionString);
+            m_mySQLClient.Open();
+        }
+
+        public void CloseConecction()
+        {
+            m_currentDataReader.Close();
+            m_currentDataReader = null;
+            m_currentMySQLCommand = null;
+
+            m_mySQLClient.Close();
+        }
+
+        public void CreateCommand(String command)
+        {
+            m_currentMySQLCommand.Cancel();
+            m_currentMySQLCommand = m_mySQLClient.CreateCommand();
+            m_currentMySQLCommand.CommandText = command;
+        }
+
+        public void CommandAddParameters(String name, String value)
+        {
+            if (m_currentMySQLCommand != null)
+                m_currentMySQLCommand.Parameters.AddWithValue(name, value);
+        }
+
+        public void CommandExecute()
+        {
+            mysqlping();
+
+            lock (m_mySQLClient)
+            {
+                m_currentDataReader = m_currentMySQLCommand.ExecuteReader();
+            }
+        }
+
+        public String[] getNextRow()
+        {
+            if (m_currentDataReader == null)
+                return new String[0];
+
+            if(m_currentDataReader.Read())
+            {
+                List<String> result = new List<String>();
+                for (int i = 0; i < m_currentDataReader.FieldCount; i++)
+                {
+                    result.Add(m_currentDataReader.GetName(i));
+                    result.Add(m_currentDataReader.GetString(i));
+                }
+
+                return result.ToArray();
+            }
+
+            return new String[0];
+        }
+
+        public void CommandExecuteNonQuery()
+        {
+            mysqlping();
+
+            lock (m_mySQLClient)
+            {
+                m_currentMySQLCommand.ExecuteReader();
+            }
+        }
+        #endregion
+
+        #region Helpers
+        private void mysqlping()
+        {
+            m_lastUse = Tools.getUnixTime();
+
+            lock (m_mySQLClient)
+            {
+                if (!m_mySQLClient.Ping())
+                    m_mySQLClient.Open();
+            }
+        }
+        #endregion
+    }
+}
---
+++ addons/Script.Functions/MySQLClient/osMySQLClient.cs
@@ -0,0 +1,160 @@
+﻿using Chris.OS.Additions.Utils;
+using Mono.Addins;
+using OpenMetaverse;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace Chris.OS.Additions.Script.Functions.MySQLClient
+{
+    [Extension(Path = "/OpenSim/RegionModules", NodeName = "RegionModule", Id = "MySQLClient")]
+
+    public class osMySQLClient : EmptyNonSharedModule
+    {
+        private List<MySqlConnectionHandler> m_mySQLHandlers = new List<MySqlConnectionHandler>();
+        private Timer m_timer = null;
+
+        #region EmptyModule
+        public override string Name
+        {
+            get { return "osMySQLClient"; }
+        }
+
+        public override void RegionLoaded(Scene scene)
+        {
+            base.Logger.Info("[" + Name + "]: Load region " + scene.Name);
+
+            base.World = scene;
+
+            IScriptModuleComms scriptModuleComms = base.World.RequestModuleInterface<IScriptModuleComms>();
+            if (scriptModuleComms != null)
+            {
+                scriptModuleComms.RegisterScriptInvocation(this, "osMySQLCreateConecction");
+
+                scriptModuleComms.RegisterScriptInvocation(this, "osMySQLOpenConecction");
+                scriptModuleComms.RegisterScriptInvocation(this, "osMySQLCloseConecction");
+                
+                scriptModuleComms.RegisterScriptInvocation(this, "osMySQLCreateCommand");
+                scriptModuleComms.RegisterScriptInvocation(this, "osMySQLCommandAddParameters");
+                scriptModuleComms.RegisterScriptInvocation(this, "osMySQLGetNextRow");
+
+                scriptModuleComms.RegisterScriptInvocation(this, "osMySQLCommandExecute");
+                scriptModuleComms.RegisterScriptInvocation(this, "osMySQLCommandExecuteNonQuery");
+            }
+
+            m_timer = new Timer();
+            m_timer.Interval = 60000;
+            m_timer.AutoReset = true;
+            m_timer.Elapsed += cleanup;
+            m_timer.Start();
+        }
+
+        public override void Close()
+        {
+            m_timer.Stop();
+            m_timer.Dispose();
+
+            foreach (MySqlConnectionHandler handler in m_mySQLHandlers)
+            {
+                handler.CloseConecction();
+            }
+        }
+
+        private void cleanup(object sender, ElapsedEventArgs e)
+        {
+            foreach(MySqlConnectionHandler handler in m_mySQLHandlers)
+            {
+                if ((handler.LastUse + 300) < Tools.getUnixTime())
+                {
+                    handler.CloseConecction();
+                }
+            }
+        }
+        #endregion
+
+        #region Script Funktions
+        [ScriptInvocation]
+        public UUID osMySQLCreateConecction(UUID hostID, UUID scriptID, String connectionString)
+        {
+            UUID newHandlerID = UUID.Random();
+            MySqlConnectionHandler newMySQLHandler = new MySqlConnectionHandler(connectionString, newHandlerID);
+            m_mySQLHandlers.Add(newMySQLHandler);
+            return newHandlerID;
+        }
+
+        [ScriptInvocation]
+        public void osMySQLOpenConecction(UUID hostID, UUID scriptID, UUID conecctionID)
+        {
+            MySqlConnectionHandler conecctionHandler = m_mySQLHandlers.Find(x => conecctionID == x.HandlerID);
+            if(conecctionHandler != null)
+            {
+                conecctionHandler.OpenConecction();
+            }
+        }
+
+        [ScriptInvocation]
+        public void osMySQLCloseConecction(UUID hostID, UUID scriptID, UUID conecctionID)
+        {
+            MySqlConnectionHandler conecctionHandler = m_mySQLHandlers.Find(x => conecctionID == x.HandlerID);
+            if (conecctionHandler != null)
+            {
+                conecctionHandler.CloseConecction();
+            }
+        }
+
+        [ScriptInvocation]
+        public void osMySQLCreateCommand(UUID hostID, UUID scriptID, String command, UUID conecctionID)
+        {
+            MySqlConnectionHandler conecctionHandler = m_mySQLHandlers.Find(x => conecctionID == x.HandlerID);
+            if (conecctionHandler != null)
+            {
+                conecctionHandler.CreateCommand(command);
+            }
+        }
+
+        [ScriptInvocation]
+        public void osMySQLCommandAddParameters(UUID hostID, UUID scriptID, String name, String value, UUID conecctionID)
+        {
+            MySqlConnectionHandler conecctionHandler = m_mySQLHandlers.Find(x => conecctionID == x.HandlerID);
+            if (conecctionHandler != null)
+            {
+                conecctionHandler.CommandAddParameters(name, value);
+            }
+        }
+
+        [ScriptInvocation]
+        public object[] osMySQLGetNextRow(UUID hostID, UUID scriptID, UUID conecctionID)
+        {
+            MySqlConnectionHandler conecctionHandler = m_mySQLHandlers.Find(x => conecctionID == x.HandlerID);
+            if (conecctionHandler != null)
+            {
+                return conecctionHandler.getNextRow();
+            }
+
+            return new object[0];
+        }
+
+        [ScriptInvocation]
+        public void osMySQLCommandExecute(UUID hostID, UUID scriptID, UUID conecctionID)
+        {
+            MySqlConnectionHandler conecctionHandler = m_mySQLHandlers.Find(x => conecctionID == x.HandlerID);
+            if (conecctionHandler != null)
+            {
+                conecctionHandler.CommandExecute();
+            }
+        }
+
+        [ScriptInvocation]
+        public void osMySQLCommandExecuteNonQuery(UUID hostID, UUID scriptID, UUID conecctionID)
+        {
+            MySqlConnectionHandler conecctionHandler = m_mySQLHandlers.Find(x => conecctionID == x.HandlerID);
+            if (conecctionHandler != null)
+            {
+                conecctionHandler.CommandExecuteNonQuery();
+            }
+        }
+        #endregion
+    }
+}
